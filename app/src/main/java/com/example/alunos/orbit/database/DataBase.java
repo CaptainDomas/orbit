374a195a86ab1fbb97446f13b148162e6cce5266
Lucasconinck <lucasconinck22@gmail.com>
2017-09-08 22:24:46 -0300

    Finalizada execução das tabelas

--- app/src/main/java/com/example/alunos/orbit/database/DataBase.java
+++ app/src/main/java/com/example/alunos/orbit/database/DataBase.java
@@ -4,17 +4,22 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.alunos.orbit.model.Linha;
+import com.example.alunos.orbit.model.Usuario;
 import com.example.alunos.orbit.model.dao.HoraDAO;
 import com.example.alunos.orbit.model.dao.HorarioDAO;
+import com.example.alunos.orbit.model.dao.LinhaDAO;
+import com.example.alunos.orbit.model.dao.TerminalDAO;
+import com.example.alunos.orbit.model.dao.UsuarioDAO;
 
 /**
  * Created by Avell 1513 on 08/09/2017.
  */
 
-public class DataBase extends SQLiteOpenHelper{
+public class DataBase extends SQLiteOpenHelper {
 
-    private static final int VERSION_NAME=1;
-    private static final String DATABASE_NAME ="orbit.sqlite";
+    private static final int VERSION_NAME = 1;
+    private static final String DATABASE_NAME = "orbit.sqlite";
 
     public DataBase(Context context) {
         super(context, DATABASE_NAME, null, VERSION_NAME);
@@ -22,9 +27,11 @@ public class DataBase extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase dataBase) {
-        dataBase.execSQL(HorarioDAO.getTabela());
         dataBase.execSQL(HoraDAO.getTabela());
-
+        dataBase.execSQL(HorarioDAO.getTabela());
+        dataBase.execSQL(TerminalDAO.getTabela());
+        dataBase.execSQL(LinhaDAO.getTabela());
+        dataBase.execSQL(UsuarioDAO.getTabela());
     }
 
     @Override
